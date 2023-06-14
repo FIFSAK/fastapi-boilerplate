797ed28ec0eaad4c58b6df1020c0ce78e76345a7
FIFSAK <anuar200572@gmail.com>
2023-06-15 00:41:47 +0600

    .env service

--- app/shanyraks/router/router_upload_file.py
+++ app/shanyraks/router/router_upload_file.py
@@ -7,21 +7,20 @@ from . import router
 # from ..adapters.s3_service import S3Service
 
 
-@router.post("/file")
-def upload_file(
-    file: UploadFile,
-    svc: Service = Depends(get_service),
-):
-    """
-    file.filename: str - Название файла
-    file.file: BytesIO - Содержимое файла
-    """
-    # url = svc.s3_service.upload_file(file.file, file.filename)
+# @router.post("/{shanyrak_id:str}/media")
+# def upload_file(
+#     file: UploadFile,
+#     svc: Service = Depends(get_service),
+# ):
+#     """
+#     file.filename: str - Название файла
+#     file.file: BytesIO - Содержимое файла
+#     """
+#     # url = svc.s3_service.upload_file(file.file, file.filename)
 
-    return {"msg": file}
+#     return {"msg": file}
 
-
-@router.post("/files")
+@router.post("/{shanyrak_id:str}/media")
 def upload_files(
     files: List[UploadFile],
     svc: Service = Depends(get_service),
@@ -30,10 +29,9 @@ def upload_files(
     file.filename: str - Название файла
     file.file: BytesIO - Содержимое файла
     """
-
-    # result = []
-    # for file in files:
-    #     url = svc.s3_service.upload_file(file.file, file.filename)
-    #     result.append(url)
+    result = []
+    for file in files:
+        url = svc.s3_service.upload_file(file.file, file.filename)
+        result.append(url)
 
     return {"msg": files}
